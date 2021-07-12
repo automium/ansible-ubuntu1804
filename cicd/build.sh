b6a58d77d7efc7bb0ba7a46df900d4cd99789ac4
Matsca09 <1948700+Matsca09@users.noreply.github.com>
2021-07-12 15:39:45 +0200

    update CI configuration and scripts

---
+++ cicd/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+# Set image name
+export IMAGE_NAME=ubuntu1804-${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA}-${CI_PIPELINE_IID}
+
+# Build image
+packer build packer.json
+sleep 10
+
+mkdir openstack
+openstack image save ${IMAGE_NAME} --file openstack/${IMAGE_NAME}.qcow2
+openstack image show -f json ${IMAGE_NAME}  | jq -r '.checksum' > openstack/${IMAGE_NAME}.qcow2.md5sum
+
+# Create container
+openstack container create automium-catalog-images
+swift post automium-catalog-images --read-acl ".r:*,.rlistings"
+
+# Upload image for openstack to swift
+swift upload automium-catalog-images openstack/${IMAGE_NAME}.qcow2
+swift upload automium-catalog-images openstack/${IMAGE_NAME}.qcow2.md5sum
+
+echo "Done"
+
